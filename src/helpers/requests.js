3b930e3ee63f44f67e0c9fb4f801778065ab7c0f
susyawan <33481540+susyawan@users.noreply.github.com>
2022-08-20 08:50:45 +0700

    update

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const accessToken = sessionStorage.getItem("access_token");
 
 const request = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://restaurant-web-heroku.herokuapp.com",
   //   headers: { Authorization: accessToken },
 });
 
